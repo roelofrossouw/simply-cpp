8c831716a58291f16688a1a15784c45effed69d7
Roelof Rossouw <roelof@1web.co.za>
2026-03-23 11:40:25 +0200

    Added color string parsing and conversion functions

--- include/color.h
+++ include/color.h
@@ -1,17 +1,30 @@
 #ifndef SC_COLOR_H
 #define SC_COLOR_H
+#include <string>
+#include <unordered_map>
 
 namespace sc {
     class color {
         double c, m, y, k;
         double r, g, b, a;
+        static const std::unordered_map<std::string, std::tuple<int, int, int> > NamedColors;
 
     public:
+        color() = default;
+
         color(double red, double green, double blue, double alpha = 1);
+
+        color(const std::string &color_name);
 
         static color from_cmyk(double cyan, double magenta, double yellow, double black, double alpha);
 
         static color from_web(int red, int green, int blue, double alpha = 1);
+
+        static color from_hex(const std::string &hex);
+
+        static color from_hsl(double x, double s, double l, double a);
+
+        static color from_string(const std::string &str);
 
         double cyan() const;
 
--- test/color.cpp
+++ test/color.cpp
@@ -5,18 +5,31 @@
 using namespace std;
 
 std::ostream &operator<<(std::ostream &lhs, const sc::color &rhs) {
-    lhs << rhs.cyan() << ", " << rhs.magenta() << ", " << rhs.yellow() << ", " << rhs.black() << ", " << rhs.alpha() << endl;
+    lhs << "cmyk(" << rhs.cyan() << "," << rhs.magenta() << "," << rhs.yellow() << "," << rhs.black() << "," << rhs.alpha() << ")"
+            << " rgb(" << rhs.red() << "," << rhs.green() << "," << rhs.blue() << "," << rhs.alpha() << ")"
+            << endl;
     return lhs;
 }
 
 int main() {
-    sc::color k = sc::color::from_web(255, 0, 0);
+    sc::color k{"darkred"};
     cout << k << endl;
 
-    k = sc::color::from_web(0, 128, 0);
+    k = sc::color::from_string("#C00");
     cout << k << endl;
 
+    k = sc::color::from_string("#c00000");
+    cout << k << endl;
 
-    k = sc::color::from_web(128, 128, 128);
+    k = sc::color::from_string("aqua");
+    cout << k << endl;
+
+    k = sc::color::from_string("hsl(120,100%,50%)");
+    cout << k << endl;
+
+    k = sc::color::from_string("rgba(255,0,200, 0.2)");
+    cout << k << endl;
+
+    k = sc::color::from_web(255, 0, 200);
     cout << k << endl;
 }

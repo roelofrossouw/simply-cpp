bb9f8045e564d0e56a5370ac5be691f9906c2101
Roelof Rossouw <roelof@1web.co.za>
2026-09-18 19:06:57 +0200

    Regenerate run.sh with the current version string

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@
 # Runs on the server, in the directory deploy.sh synced to.
 
 module="sc-core"
-version="1.1.8"
+version="1.1.9"
 ubuntu_codename=$(lsb_release -sc)
 build_directory="cmake-build-$ubuntu_codename"
 apt -y install cmake clang-tidy ccache g++

e930efcc3864433440924c9f0850fd35c4034601
Roelof Rossouw <roelof@1web.co.za>
2026-09-24 15:00:37 +0200

    Tag bump

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,7 @@
 # Runs on the server, in the directory deploy.sh synced to.
 
 module="sc-core"
-version="1.2.1"
+version="1.2.2"
 ubuntu_codename=$(lsb_release -sc)
 build_directory="cmake-build-$ubuntu_codename"
 apt -y install cmake clang-tidy ccache g++
